353789f82e79d9098d1e1f4daad2b5b4e8c7b918
Chris Copley <CopOnTheRun@gmail.com>
2024-04-01 11:00:36 -0400

    Adds --property flag to cli

--- btrview/scripts/btrview.py
+++ btrview/scripts/btrview.py
@@ -31,9 +31,14 @@ def parser() -> argparse.ArgumentParser:
             choices = ("root","deleted","unreachable"),
             default = ("root","unreachable"))
 
+    arg_parser.add_argument(
+            "--property",
+            help = "The subvolume property to print out in the tree",
+            default = None)
+
     return arg_parser
 
-def logic(labels: list[str], snapshots, root, deleted, unreachable) -> None:
+def logic(labels: list[str], snapshots, root, deleted, unreachable, props) -> None:
     check_root()
     filesystems = Btrfs.get_filesystems(labels)
     for fs in filesystems:
@@ -44,14 +49,17 @@ def logic(labels: list[str], snapshots, root, deleted, unreachable) -> None:
         heading = "Snapshots:" if snapshots else "Subvolumes:"
         print(heading)
         for tree in fs.forest(snapshots, root, deleted, unreachable):
-            print(textwrap.indent(str(tree).strip(),"  "))
+            #stdout=False is only needed because of bug
+            #see https://github.com/caesar0301/treelib/issues/221
+            tree = tree.show(data_property=props, stdout=False).strip()
+            print(textwrap.indent(tree,"  "))
 
 def main():
     args = parser().parse_args()
     root = "root" in args.include
     deleted = "deleted" in args.include
     unreachable = "unreachable" in args.include
-    logic(args.labels, args.snapshots, root ,deleted, unreachable)
+    logic(args.labels, args.snapshots, root ,deleted, unreachable, args.property)
     
 if __name__ == "__main__":
     main()
