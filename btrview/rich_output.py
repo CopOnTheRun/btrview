b65b61b072cb07c3f88aacb77b361ab0f4b70482
Chris Copley <CopOnTheRun@gmail.com>
2024-04-09 19:32:59 -0400

    Refactor rich_output.py

--- btrview/rich_output.py
+++ btrview/rich_output.py
@@ -9,36 +9,51 @@ from rich.table import Table
 from btrview.btrfs import Btrfs, get_forest
 from btrview.subvolume import Subvolume
 
-def logic(labels: list[str], root, deleted, unreachable, prop, export) -> str | None:
+def create_rich_table(title: str, subvol_forest: Group,snapshot_forest: Group) -> Table:
+    """Creates a table from a subvolume forest and snapshot forest"""
+    forest_table = Table(title = f"{title}", show_edge=False,
+                         show_lines=False,expand=True,box=None,padding=0)
+    forest_table.add_column("Subvolume Tree:")
+    forest_table.add_column("Snapshot Tree:")
+    forest_table.add_row(subvol_forest,snapshot_forest)
+    return forest_table
+
+def logic(labels: list[str], root: bool, deleted: bool,
+          unreachable:bool , prop: str, export: str) -> str:
     """Constructs Rich output based on the parameters given."""
     filesystems = Btrfs.get_filesystems(labels)
-    console = Console(record = True)
-    captures = []
+    tables = []
     for fs in filesystems:
         subvols = fs.subvolumes(root,deleted,unreachable)
+
         subvol_forest = get_forest(subvols,"subvol")
         subvol_forest = rich_forest(subvol_forest, prop)
+
         snapshot_forest = get_forest(subvols,"snap")
         snapshot_forest = rich_forest(snapshot_forest, prop)
 
-        forest_table = Table(title = f"{fs}", show_edge=False,
-                             show_lines=False,expand=True,box=None,padding=0)
-        forest_table.add_column("Subvolume Tree:")
-        forest_table.add_column("Snapshot Tree:")
-        forest_table.add_row(subvol_forest,snapshot_forest)
+        forest_table = create_rich_table(str(fs),subvol_forest,snapshot_forest)
+        tables.append(forest_table)
+    return create_table_output(tables, export)
 
+
+def create_table_output(tables: list[Table], fmt: str | None) -> str:
+    console = Console(record = True)
+    captures = []
+    for table in tables:
         with console.capture() as capture:
-            console.print(forest_table)
+            console.print(table)
         captures.append(capture.get())
-    match export:
+    match fmt:
         case "svg":
-            return console.export_svg()
+            out_str =  console.export_svg()
         case "text":
-            return console.export_text()
+            out_str = console.export_text()
         case "html":
-            return console.export_html()
+            out_str = console.export_html()
         case _:
-            return "".join(captures)
+            out_str = "".join(captures)
+    return out_str
 
 def treelib_to_rich(tree: treelib.Tree,
                     node: treelib.Node,
