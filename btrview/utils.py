0f3a120ba8333214d197914ec27ed3b62576ea35
Chris Copley <CopOnTheRun@gmail.com>
2024-05-27 10:47:41 -0400

    Make run function print error if encountered

--- btrview/utils.py
+++ btrview/utils.py
@@ -20,6 +20,7 @@ def run(command: str, **kwargs) -> subprocess.CompletedProcess[str]:
         out = subprocess.run(tokens, check=True, capture_output=True, 
                              text=True, **kwargs)
     except subprocess.CalledProcessError as e:
+        print(e.stderr)
         raise e
     return out
 
